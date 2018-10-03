f9861f965a6dd24a1040b54c861ce5e99383a9b1
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2018-10-03 17:21:45 +0200

    Formatted printing

--- base.go
+++ base.go
@@ -1,9 +1,12 @@
 package gotversion
 
 import (
+	"fmt"
+
 	version "github.com/hashicorp/go-version"
 )
 
+// Base is a (potential) base version
 type Base struct {
 	*version.Version
 	Branch   string
@@ -11,7 +14,12 @@ type Base struct {
 	Offset   int64
 }
 
+// BaseCollection implements the Sortable interface
 type BaseCollection []*Base
+
+func (b Base) String() string {
+	return fmt.Sprintf("semver=%s branch=%s offset=%d", b.Version.String(), b.Branch, b.Offset)
+}
 
 func (v BaseCollection) Len() int {
 	return len(v)
